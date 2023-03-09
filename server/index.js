5e02232270fb484780e098cb83502de099fae6ba
Bryce Bresnan <Bryce.Bresnan@gmail.com>
2023-03-08 23:50:07 -0800

    test code

--- server/index.js
+++ server/index.js
@@ -5,8 +5,42 @@ const PORT = process.env.PORT || 3001;
 const path = require('path')
 const app = express();
 
-const Timer = new ApiCallTimer([],10000);
-Timer.getAuthToken()
+let testThreadList = [
+  {
+    displayName: "cats",
+    accountsActive: null,
+    subscribers: null,
+    activeScore: null,
+    scoreThreshold:30,
+    isHot: false,
+    id: 1
+  },
+  {
+    displayName: "dogs",
+    accountsActive: null,
+    subscribers: null,
+    activeScore: null,
+    scoreThreshold:30,
+    isHot: false,
+    id: 2
+  },
+  {
+    displayName: "cringe",
+    accountsActive: null,
+    subscribers: null,
+    activeScore: null,
+    scoreThreshold:30,
+    isHot: false,
+    id: 3
+  }
+]
+
+const Timer = new ApiCallTimer(testThreadList,10000);
+Timer.run();
+setTimeout(() => {Timer.loopApiCall()}, 500)
+setTimeout(() => {console.log(Timer.mainThreadList)}, 1000)
+// Timer.getAuthToken()
+// Timer.apiCall(testThread)
 
 app.use(express.static(path.join(__dirname, '../../reddalert/build')))
 
