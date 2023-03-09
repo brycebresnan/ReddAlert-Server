7a27f3c9fa683aed4acafbc6a7d0ab1fb2da790b
Bryce Bresnan <Bryce.Bresnan@gmail.com>
2023-03-08 22:57:59 -0800

    add make api call function

--- server/js/ApiCallTimer.js
+++ server/js/ApiCallTimer.js
@@ -3,11 +3,21 @@ require("dotenv").config()
 module.exports = class ApiCallTimer {
 
   constructor(threadList, interval) {
-    this.threadList = threadList;
+    this.mainThreadList = threadList;
     this.interval = interval;
     this.token = null;
     this.intId = null;
     this.authError = null;
+    this.apiError = null;
+    this.thread = {
+      displayName: null,
+      accountsActive: null,
+      subscribers: null,
+      activeScore: null,
+      scoreThreshold:30,
+      isHot: false,
+      id: v4()
+    }
   }
 
   startTimer = (interval=600000) => {
@@ -49,8 +59,40 @@ module.exports = class ApiCallTimer {
 
   }
 
-  apiCall = (token) => {
-    return
+  apiCall = (threadObj) => {
+    let newThreadObj = threadObj;
+
+    if (this.token == null) {
+      this.apiError = "Authentication Token missing or undefined"
+      return
+    }
+    
+    fetch(`https://oauth.reddit.com/r/${newThreadObj.displayName}/about`, { headers: {Authorization: `Bearer ${this.token}`}})
+    .then(response => {
+      if (!response.ok) {
+        throw new Error(`${response.status}: ${response.statusText}`);
+      } else {
+        return response.json()
+      }
+    })
+    .then((jsonifiedResponse) => {
+      let calcActiveScore = (jsonifiedResponse.data.accounts_active / jsonifiedResponse.data.subscribers) * 10000;
+      newThreadObj.accountsActive = jsonifiedResponse.data.accounts_active;
+      newThreadObj.displayName = jsonifiedResponse.data.display_name;
+      newThreadObj.subscribers = jsonifiedResponse.data.subscribers;
+      newThreadObj.activeScore = calcActiveScore;
+      if (calcActiveScore >= newThread.scoreThreshold){
+        newThreadObj.isHot = true;
+      }
+      const newMainThreadList = this.mainThreadList.concat(newThreadObj); 
+      this.mainThreadList = newMainThreadList;
+    })
+    .catch((error) => {
+      this.apiError = error.message
+      newThreadObj.error = error.message
+      const newMainThreadList = this.mainThreadList.concat(newThreadObj); 
+      this.mainThreadList = newMainThreadList;
+    });
   }
 
 }
